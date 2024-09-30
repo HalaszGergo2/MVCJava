99f1fbe09095e73f35fddb5c84aa189008082c6c
HalaszGergo2 <xlomox12@gmail.com>
2024-09-30 15:42:30 +0200

    MVC - Konzol nezet

--- src/modell/JatekModell.java
+++ src/modell/JatekModell.java
@@ -1,25 +1,20 @@
 package modell;
 
-import java.util.Scanner;
+public class JatekModell {
 
-
-
-public class Jatek {
-    private static final Scanner sc = new Scanner(System.in);
     
     private Lada[] ladak;
     private int valasztas;
 
-    public Jatek() {
+    public JatekModell() {
         valasztas = -1;
         ladak = new Lada[3];
         ladak[0] = new Lada("arany", "Én rejtem a kincset!");
         ladak[1] = new Lada("ezüst", "Nem én rejtem a kincset!", true);
         ladak[2] = new Lada("bronz", "Az arany hazudik");
         
-        megjelenit(kezdes());
-        bekeres();
-        megjelenit(ellenorzes());
+        //bekeres();
+        //megjelenit(ellenorzes());
     }
 
     public String kezdes() {
@@ -33,16 +28,15 @@ public class Jatek {
         return s;
     }
 
-    private void bekeres() {
-        megjelenit("Melyik ládában van a kincs? (1,2,3): ");
-        valasztas = sc.nextInt()-1;
+    public int getValasztas() {
+        return valasztas;
     }
 
-    private void megjelenit(String uzenet) {
-        System.out.print(uzenet);
+    public void setValasztas(int valasztas) {
+        this.valasztas = valasztas;
     }
 
-    private String ellenorzes() {
+    public String ellenorzes() {
         String talalat = "Gratulálok, jól választottál!";
         String nemTalalt = "Sajnios nem talált!";
         return ladak[valasztas].isKincs() ? talalat : nemTalalt;
