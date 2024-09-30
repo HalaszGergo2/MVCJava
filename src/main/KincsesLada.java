b16f33c12d6af7699ef00a5b045a894077ab6d4b
HalaszGergo2 <xlomox12@gmail.com>
2024-09-30 16:05:42 +0200

    uj JOP nezet

--- src/main/KincsesLada.java
+++ src/main/KincsesLada.java
@@ -1,7 +1,9 @@
 package main;
 
 import modell.JatekModell;
+import nezet.FelugroNezet;
 import nezet.KonzolNezet;
+import vezerlo.FelugroVezerlo;
 import vezerlo.KonzolVezerlo;
 
 
@@ -13,8 +15,12 @@ public class KincsesLada {
         
         JatekModell modell = new JatekModell();
         KonzolNezet nezet = new KonzolNezet();
+        FelugroNezet felNezet = new FelugroNezet();
         
-        new KonzolVezerlo(modell, nezet);
+        //new KonzolVezerlo(modell, nezet);
+        
+        new FelugroVezerlo(modell, felNezet);
+        
         
         
     }
